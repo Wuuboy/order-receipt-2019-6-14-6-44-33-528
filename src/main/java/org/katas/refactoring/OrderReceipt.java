cbe5881372ca97880e71f561e9a1fe7b03cdcd58
曹桂凤 <demi.cao@oocl.com>
2019-07-16 17:36:29 +0800

    [Demi]refactor not finished

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -9,6 +9,16 @@ public class OrderReceipt {
         this.order = order;
     }
 
+    final ArrayList<LineItem> lineItems = order.getLineItems();
+
+    final double salesTax = lineItems.stream().mapToDouble(e -> {
+        return e.getTotalAmount()*0.10;
+    }).sum();
+
+    final double totalAmount = lineItems.stream().mapToDouble(e -> {
+        return e.getTotalAmount()*0.10;
+    }).sum();
+
     public String[] printLineItemAndSalesTax() {
         String[] outputArray = new String[order.getLineItems().size()+2];
         double totalSalesTax = 0d;
@@ -16,7 +26,7 @@ public class OrderReceipt {
         ArrayList<LineItem> lineItems = order.getLineItems();
         for (int i = 0;i<order.getLineItems().size();i++) {
             outputArray[i] = String.format("%s\t%.1f\t%d\t%.1f\n",lineItems.get(i).getDescription(),lineItems.get(i).getPrice(),lineItems.get(i).getQuantity(),lineItems.get(i).getTotalAmount());
-            double salesTax = caculateSsalesTax(lineItems.get(i).getTotalAmount() , 0.10);
+            double salesTax = caculateSalesTax(lineItems.get(i).getTotalAmount() , 0.10);
             totalSalesTax += salesTax;
             totalAmount += lineItems.get(i).getTotalAmount() + salesTax;
         }
@@ -25,7 +35,19 @@ public class OrderReceipt {
         return outputArray;
     }
 
-    public double caculateSsalesTax(double totalAmount,double rate){
+//    public String appendLineItems(ArrayList<LineItem> lineItems){
+//        String[] outputArray = new String[order.getLineItems().size()+2];
+//        double totalSalesTax = 0d;
+//        double totalAmount = 0d;
+//        for (int i = 0;i<order.getLineItems().size();i++) {
+//            outputArray[i] = String.format("%s\t%.1f\t%d\t%.1f\n",lineItems.get(i).getDescription(),lineItems.get(i).getPrice(),lineItems.get(i).getQuantity(),lineItems.get(i).getTotalAmount());
+//            double salesTax = caculateSsalesTax(lineItems.get(i).getTotalAmount() , 0.10);
+//            totalSalesTax += salesTax;
+//            totalAmount += lineItems.get(i).getTotalAmount() + salesTax;
+//        }
+//    }
+
+    public double caculateSalesTax(double totalAmount,double rate){
         return totalAmount*rate;
     }
 
