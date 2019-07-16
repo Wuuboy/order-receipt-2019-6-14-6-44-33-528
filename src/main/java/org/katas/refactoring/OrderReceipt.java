5ab34ba995cda687892fc50c8522e68c3f764b46
Wuuboy <30542499+Wuuboy@users.noreply.github.com>
2019-07-16 23:51:14 +0800

    Update OrderReceipt.java

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -9,16 +9,6 @@ public class OrderReceipt {
         this.order = order;
     }
 
-    final ArrayList<LineItem> lineItems = order.getLineItems();
-
-    final double salesTax = lineItems.stream().mapToDouble(e -> {
-        return e.getTotalAmount()*0.10;
-    }).sum();
-
-    final double totalAmount = lineItems.stream().mapToDouble(e -> {
-        return e.getTotalAmount()*0.10;
-    }).sum();
-
     public String[] printLineItemAndSalesTax() {
         String[] outputArray = new String[order.getLineItems().size()+2];
         double totalSalesTax = 0d;
@@ -35,17 +25,6 @@ public class OrderReceipt {
         return outputArray;
     }
 
-//    public String appendLineItems(ArrayList<LineItem> lineItems){
-//        String[] outputArray = new String[order.getLineItems().size()+2];
-//        double totalSalesTax = 0d;
-//        double totalAmount = 0d;
-//        for (int i = 0;i<order.getLineItems().size();i++) {
-//            outputArray[i] = String.format("%s\t%.1f\t%d\t%.1f\n",lineItems.get(i).getDescription(),lineItems.get(i).getPrice(),lineItems.get(i).getQuantity(),lineItems.get(i).getTotalAmount());
-//            double salesTax = caculateSsalesTax(lineItems.get(i).getTotalAmount() , 0.10);
-//            totalSalesTax += salesTax;
-//            totalAmount += lineItems.get(i).getTotalAmount() + salesTax;
-//        }
-//    }
 
     public double caculateSalesTax(double totalAmount,double rate){
         return totalAmount*rate;
